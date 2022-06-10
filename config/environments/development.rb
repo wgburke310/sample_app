b598c2f7682aa4bf65e93f476b77142c4a3aff8a
Ubuntu <ubuntu@ip-172-31-57-208.ec2.internal>
2022-06-10 14:57:16 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '<hex string>.vfs.cloud9.us-east-2.amazonaws.com'
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
